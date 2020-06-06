3e748253ff7dede445df32f4478e3936a4b8b274
jasleenkapoor <zhap062@live.rhul.ac.uk>
2020-06-06 18:31:57 +0000

    the code

--- main.py
+++ main.py
@@ -1,17 +1,29 @@
-import numpy as np 
-import matplotlib.pyplot as plt 
-
-f = open('values.txt', 'r') 
+import numpy as np
+import matplotlib.pyplot as plt
+f = open("values.txt", "r") ## opens up and reads the file
 f.readline()
-x =  []
-y=   []
+
+r = []
+v = []
+dR = []
+dV = []
+m = []
+predictedV = []
+G = 4.3*(10**-6)
 
 for line in f:
-  x.append(float(line.split('\t')[0]))
-  y.append(float(line.split('\t')[1]))
+  r.append(float(line.split("\t")[0]))
+  v.append(float(line.split("\t")[1]))
+  dR.append(line.split("\t")[2])
+  dV.append(float(line.split("\t")[3]))
+  m.append(float(line.split("\t")[4]))
+  predictedV.append(((float(G))*(float(line.split('\t')[4]))/(float(line.split('\t')[0])))**(1/2))
+
+x = np.array(r)
+y = np.array(v)
+y1 = np.array(predictedV)
+
 
 plt.plot(x,y)
-plt.title('A graph of velocity(km/h) against radius(kpc)')
-plt.xlabel('radius(kpc)')
-plt.ylabel('velocity(km/h)')
+plt.plot(x,y1)
 plt.show()
